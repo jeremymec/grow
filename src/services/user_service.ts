03576eb23ef3d80e276bfa281ac59eca942833c2
Jeremy Mechen <jeremymechen1997@gmail.com>
2023-03-31 01:03:38 -0700

    Update user service

--- src/services/user_service.ts
+++ src/services/user_service.ts
@@ -1,18 +1,13 @@
 import { User } from "@/models/user";
 import { getDBConnection } from "../database";
-
+import { connect, Connection } from '@planetscale/database'
 const conn = getDBConnection();
 
 const listUsers = async (): Promise<User[]> => {
-    const query = conn.execute('SELECT * FROM users');
-
-    const data = query.then(res => {
-        return res.rows.map(row => {
-            return {
-                id: 
-            }
-        })
-    });
+    const query = await conn.execute('SELECT * FROM users;');
+    const users = query.rows as User[];
+    console.log(users[0].code);
+    return query.rows.map(row => row as User);
 }
 
 export { listUsers }
